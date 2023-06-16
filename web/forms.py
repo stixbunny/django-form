803e68d7994f0172f98e90e5690ebe0cb114fffe
Milenko Castillo <milenko.castillo@gmail.com>
2023-06-16 19:30:18 -0400

    form field names fix and styling

--- web/forms.py
+++ web/forms.py
@@ -3,10 +3,13 @@ from django import forms
 
 class MessageForm(forms.Form):
     text = forms.CharField(
+        label = "Mensaje",
         max_length=200,
-        widget=forms.Textarea(attrs={"class": "form-control"}),
+        widget=forms.Textarea(attrs={"class": "form-control", "rows":5}),
     )
     author = forms.CharField(
+        label = "Autor",
+        required = False,
         max_length=50,
         widget=forms.TextInput(attrs={"class": "form-control"}),
     )
--- web/models.py
+++ web/models.py
@@ -2,6 +2,6 @@ from django.db import models
 
 
 class Message(models.Model):
-    text = models.CharField(max_length=200)
+    text = models.CharField("Mensaje", max_length=200)
     pub_date = models.DateTimeField("date published", auto_now_add=True, blank=True)
-    author = models.CharField(max_length=50, blank=True, null=True)
+    author = models.CharField("Autor", max_length=50, blank=True, null=True)
