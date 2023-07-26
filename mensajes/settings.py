1d292375472257d93b2424b26b976277e15246e1
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-26 11:09:56 -0400

    static files

--- mensajes/settings.py
+++ mensajes/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from os import getenv, path
+from os import getenv
 import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
