d426893cb86ed3d14efb48b2668826bda506760a
Milenko Castillo <milenko.castillo@gmail.com>
2023-07-26 12:42:28 -0400

    whitenoise storage

--- mensajes/settings.py
+++ mensajes/settings.py
@@ -132,3 +132,5 @@ MEDIA_ROOT = BASE_DIR / 'uploads'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
